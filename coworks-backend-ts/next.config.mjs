f59b82cbf81ee517bfcfbd78692bc7ea14773337
Rajulraj Ramesan <20BTRCL023@jainuniversity.ac.in>
2025-03-07 10:53:36 +0530

    t1

--- coworks-backend-ts/next.config.mjs
+++ coworks-backend-ts/next.config.mjs
@@ -1,13 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      serverComponentsExternalPackages: ['sequelize', 'pg', 'pg-hstore'],
-    },
-    webpack: (config) => {
-      // This makes Webpack treat pg-native as an external dependency
-      config.externals.push('pg-native');
-      return config;
-    },
-  };
-  
-  export default nextConfig;
+  experimental: {
+    serverComponentsExternalPackages: ['sequelize', 'pg', 'pg-hstore'],
+  },
+  webpack: (config) => {
+    // Handle path aliases
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, 'src'),
+    };
+    
+    // This makes Webpack treat pg-native as an external dependency
+    config.externals.push('pg-native');
+    return config;
+  },
+};
+
+export default nextConfig;
